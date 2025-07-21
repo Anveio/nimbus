28d1bd8879fbe5da3cfdde8575366b869d583e80
Shovon Hasan <im.shovonhasan@gmail.com>
2025-07-21 13:51:29 -0700

    Small optimizations.

--- packages/protocol/vite.config.ts
+++ packages/protocol/vite.config.ts
@@ -1,16 +1,13 @@
-import { dirname, resolve } from 'path'
-import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'ManaSshProtocol',
+      entry: './src/index.ts',
+      name: 'ManaSSHWebProtocol',
+      formats: ['es', 'umd'],
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
       external: ['mana-ssh-websocket'],
