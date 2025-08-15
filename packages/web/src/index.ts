6f46232a9d6feff12a95af217796ab6bd9560694
Shovon Hasan <im.shovonhasan@gmail.com>
2025-08-15 10:52:20 -0700

    .

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,5 +1,2 @@
 // Main user-facing API will go here.
-import '@mana-ssh/protocol';
-import '@mana-ssh/websocket';
-
-console.log("ManaSSH Web Client Loaded");
+console.log('ManaSSH Web Client Loaded')
