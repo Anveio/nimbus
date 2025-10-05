912b52d42088adc404736225523effa828cb12a1
Shovon Hasan <im.shovonhasan@gmail.com>
2025-10-05 00:35:34 -0700

    Attempt #2 at setting up an electron-demo.

--- apps/electron-terminal/src/renderer/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import { ElectronTerminalApp } from './app'
-
-const rootElement = document.getElementById('root')
-if (!rootElement) {
-  throw new Error('Failed to locate root container')
-}
-
-createRoot(rootElement).render(<ElectronTerminalApp />)
